f05fbcfc4fb33ca63812f7b679ee10910120a7f1
Hirusha2000 <2000hirushaudara@gmail.com>
2023-04-12 19:11:39 +0530

    login form updates

--- src/AppInitializer.java
+++ src/AppInitializer.java
@@ -1,6 +1,7 @@
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import lk.ijse.Hostel_Management_System.bo.BOFactory;
 import lk.ijse.Hostel_Management_System.bo.custom.LoginBO;
@@ -8,6 +9,7 @@ import lk.ijse.Hostel_Management_System.entity.Student;
 import lk.ijse.Hostel_Management_System.util.FactoryConfiguration;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+
 
 import java.time.LocalDate;
 
@@ -21,15 +23,14 @@ public class AppInitializer extends Application {
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("lk/ijse/Hostel_Management_System/view/LoginForm.fxml"))));
         primaryStage.setTitle("D24 HOSTEL MANAGEMENT SYSTEM powered by HUG SOLUTIONS@2023");
+       /* Image image =new Image("view/assests/images/d24.PNG");
+       primaryStage.getIcons().add(image);*/
+        primaryStage.setMaximized(true);
+        primaryStage.setResizable(false);
         primaryStage.show();
 
 
-       /* Image image=new Image("assets/restaurant.logo.png");
-        primaryStage.getIcons().add(image);
-        primaryStage.setMaximized(true);
 
-        primaryStage.setResizable(false);
-        primaryStage.show();*/
 
     }
 }
